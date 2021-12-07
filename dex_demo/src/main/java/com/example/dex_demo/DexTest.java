5eb76aaefe3585904edb46558636f260beb30e7e
han1202012 <octopus_truth@163.com>
2021-12-07 22:32:28 +0800

    update

---
+++ dex_demo/src/main/java/com/example/dex_demo/DexTest.java
@@ -0,0 +1,12 @@
+package com.example.dex_demo;
+
+import android.util.Log;
+
+/**
+ * 测试 DEX 字节码加载并执行
+ */
+public class DexTest {
+    public void test(){
+        Log.i("DexTest", "DexTest : Hello World!!!");
+    }
+}
